a996ebf198b35cab939d9270c2b4c6c93378c1d0
Joe Carmichael <joe@2spring.net>
2026-05-09 12:54:33 +0100

    chore: ignore mocks

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,28 +1,25 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-    test: {
-        clearMocks: true,
-        exclude: ["node_modules"],
-        coverage: {
-            provider: "v8",
-            reportsDirectory: "coverage",
-            reporter: ["text", "json-summary", "json", "lcovonly"],
-            include: ["src/**/*.ts"],
-            exclude: [
-                "**/*.spec.ts",
-                "**/*.test.ts",
-            ],
-            thresholds: {
-                statements: 100,
-                branches: 100,
-                functions: 100,
-                lines: 100,
-            },
-        },
-        pool: "threads",
-        isolate: false,
-        maxWorkers: 1,
-        globals: false,
+  test: {
+    clearMocks: true,
+    exclude: ["node_modules"],
+    coverage: {
+      provider: "v8",
+      reportsDirectory: "coverage",
+      reporter: ["text", "json-summary", "json", "lcovonly"],
+      include: ["src/**/*.ts"],
+      exclude: ["**/__mocks__/**", "**/*.spec.ts", "**/*.test.ts"],
+      thresholds: {
+        statements: 100,
+        branches: 100,
+        functions: 100,
+        lines: 100,
+      },
     },
+    pool: "threads",
+    isolate: false,
+    maxWorkers: 1,
+    globals: false,
+  },
 });
